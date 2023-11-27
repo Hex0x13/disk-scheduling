6e4761a7839e935fc42a74ee35ddec2207661c29
Jay0x13 <dilucrag19@gmail.com>
2023-11-27 23:02:07 +0800

    update

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ array = []
 root = ctk.CTk()
 root.geometry("800x600")
 
+headerframe = ctk.CTkFrame(root)
 outputframe = ctk.CTkFrame(root)
 outputframe.pack(expand=True, fill='both')
 arrayframe = ctk.CTkFrame(root)
@@ -22,14 +23,12 @@ def validate_index_input(value):
 
 valueframe = ctk.CTkFrame(navframe)
 valueframe.grid(row=0, column=1)
-validation = valueframe.register(validate_input)
-validation2 = valueframe.register(validate_index_input)
 indexentry = ctk.CTkEntry(
     valueframe, 
     justify='center', 
     placeholder_text='top',
     validate='key',
-    validatecommand=(validation2, '%P')
+    validatecommand=(valueframe.register(validate_index_input), '%P')
     )
 indexentry.grid(row=0, column=0)
 
@@ -38,7 +37,7 @@ elementry = ctk.CTkEntry(
     justify='center', 
     placeholder_text='New Element',
     validate='key',
-    validatecommand=(validation, '%P')
+    validatecommand=(valueframe.register(validate_input), '%P')
     )
 elementry.grid(row=0, column=1)
 
@@ -47,8 +46,21 @@ def destroy_children(widget: ctk.CTkFrame):
     for child in children:
         child.destroy()
 
-arrayparent = ctk.CTkFrame(arrayframe, height=50, fg_color='transparent')
+arrayparent = ctk.CTkFrame(arrayframe, height=80, fg_color='transparent')
 arrayparent.pack(anchor=ctk.CENTER)
+
+def validate_array_input(value):
+    return value.isdigit() or value == ''
+
+def edit_array(*args, var, index):
+    global array
+    if var.get() != '':
+        array[index] = int(var.get())
+
+def remove_element(index):
+    global array
+    array.pop(index)
+    display_array()
 
 def display_array():
     global arrayparent
@@ -56,14 +68,52 @@ def display_array():
     destroy_children(arrayparent)
     
     for k, v in enumerate(array):
-        frame = ctk.CTkFrame(arrayparent, border_color='white', border_width=1)
-        frame.grid(row=0, column=k)
-        text = ctk.CTkLabel(frame, text=v, fg_color='transparent', font=ctk.CTkFont(size=10))
-        text.pack(pady=5, padx=20)
+        deletebutton = ctk.CTkButton(
+            arrayparent, text='remove',
+            width=36, height=10, 
+            font=ctk.CTkFont(size=8), 
+            fg_color='transparent', 
+            border_width=1, 
+            border_color='white',
+            command=lambda index=k: remove_element(index)
+        )
+        deletebutton.grid(row=0, column=k, ipady=0, pady=5)
+
+        var = ctk.StringVar()
+        element = ctk.CTkEntry(
+            arrayparent,
+            textvariable=var,
+            font=ctk.CTkFont(size=12),
+            border_color='white',
+            border_width=1,
+            fg_color='transparent',
+            width=40,
+            height=40,
+            justify='center',
+            validate='key',
+            validatecommand=(arrayparent.register(validate_array_input), '%P')
+        )
+        element.insert(0, v)
+        element.grid(row=1, column=k)
+        def on_focus_out(event):
+            if element.get() == '':
+                element.insert(0, array[k])
+        element.bind("<FocusOut>", on_focus_out)
+        var.trace('w', lambda *args, index=k, var=var, elem=element, val=v: edit_array(args, index=index, var=var))
+        # deletebutton = ctk.CTkButton(arrayparent, text='remove', font=ctk.CTkFont(size=10), height=12, width=36)
+        # deletebutton.grid(row=0, column=k)
+
+        # editbutton = ctk.CTkButton(arrayparent, text='edit', font=ctk.CTkFont(size=10), height=12, width=36)
+        # editbutton.grid(row=1, column=k)
+
+        # frame = ctk.CTkFrame(arrayparent, border_color='white', border_width=1, width=40, height=40)
+        # frame.grid(row=2, column=k)
+        # text = ctk.CTkLabel(frame, text=v, fg_color='transparent', font=ctk.CTkFont(size=12))
+        # text.pack(pady=5, padx=20)
     
     for i in range(len(array)):
-        text = ctk.CTkLabel(arrayparent, text=f'{i+1}', font=ctk.CTkFont(size=8))
-        text.grid(row=1, column=i)
+        text = ctk.CTkLabel(arrayparent, text=f'{i+1}', font=ctk.CTkFont(size=10))
+        text.grid(row=2, column=i)
 
 def addElement():
     global indexentry
